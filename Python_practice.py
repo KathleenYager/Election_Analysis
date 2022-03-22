e684933dad8f7e6715842a095bd053570f34b368
KathleenYager <kyager@ccdocle.org>
2022-03-21 22:31:25 -0400

    Adding Python module 3 files

---
+++ Python_practice.py
@@ -0,0 +1,46 @@
+print("Hello World")
+print("Hello World!")
+type("Hello World")
+age = 3
+print(age)
+counties = ["Arapahoe", "Denver", "Jefferson"]
+
+print(counties)
+# myList = ["Jacob", 25, "Ahemd", 80]
+# print(myList)
+# myList.append("Matt")
+# print(myList)
+# print(len(myList))
+list()
+counties
+counties[0]
+counties[2]
+print(counties[2])
+print(counties[0])
+print(counties[-1])
+print(counties[-2])
+len(counties)
+counties[0:2]
+counties[:2]
+#list.append("Cleveland","Akron", "North Ridgeville")
+print(counties)
+counties.append("El Paso")
+print(counties)
+counties
+counties.pop(3)
+print(counties)
+counties[2] = "El Paso"
+counties[0:2]
+myList = ["El Paso", "Jefferson", "Denver", "Arapahoe"]
+print(myList)
+counties_dict = {'Arapahoe: 422829','Denver: 463353', 'Jefferson: 432438'}
+len(counties_dict)
+counties_dict.get("Denver")
+counties_dict["Arapahoe"]
+voting_data = []
+voting_data.append({"county":"Arapahoe","registered_voters":422829})
+voting_data.append({"county":"Denver","registered_voters":462253})
+voting_data.append({"county":"Jefferson","registered_voters":432438})
+voting_data = []
+
+
